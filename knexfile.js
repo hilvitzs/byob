f60e46f5498e422d56e1132741941caae72b0430
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-07-14 12:28:26 -0600

    fix linter errors

--- knexfile.js
+++ knexfile.js
@@ -24,14 +24,14 @@ module.exports = {
   },
 
   production: {
-  client: 'pg',
-  connection: process.env.DATABASE_URL + `?ssl=true`,
-  migrations: {
-    directory: './db/migrations'
-  },
-  seeds: {
-    directory: './db/seeds/dev'
-  },
-  useNullAsDefault: true
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
   }
 };
